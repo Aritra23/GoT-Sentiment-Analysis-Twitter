ef1198c8c765cef5972bdd22b970a5231a2066c0
Sandeep <sandeep29031984@gmail.com>
2019-05-11 13:53:52 -0700

    Adding files

---
+++ tweepy_batch.py
@@ -0,0 +1,54 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from bs4 import BeautifulSoup
+import requests
+import pprint
+import json
+import pymongo
+from sqlalchemy import func
+
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+# Define the 'classDB' database in Mongo
+db = client.projDB
+tweet = db.tweet.find()
+
+#Variables that contains the user credentials to access Twitter API
+access_token = "114010579-UscavtyluJ4ZOjGBGPwF3Xy8i5ynDgmeiYRLQ9WL"
+access_token_secret = "hnA5I14kCwovdQF1Ciyz3VWOJlPlre8jHEKoeb9OshhJe"
+consumer_key = "j2BhP2Q3LFCzpPQ5o0Xx4GuY7"
+consumer_secret = "vkcmHL1U71cpqdn1LBl7GY4cdJIkvFotKAyZeChIuHBoq5aQ2j"
+
+#This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+   def on_data(self, data):
+    # Insert a document into the 'tweet' collection
+        data = json.loads(data)
+        if data["lang"] == 'en':
+            db.tweet.insert_one(
+                {
+                'id': int(data["id"]),
+                'text': data["text"],
+                'language': data["lang"],
+                'created': data["created_at"],
+                'entities' : data["entities"],
+                'place' : data["place"],
+                'source' : data["source"]
+                })
+#         print(data)
+        return True
+
+   def on_error(self, status):
+       print(status)
+
+
+#This handles Twitter authetification and the connection to Twitter Streaming API
+l = StdOutListener()
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+stream = Stream(auth, l)
+
+#This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+stream.filter(track=['Game of Thrones', 'A Song of Ice and Fire', 'Iron Thrones'])
+stream.filter(track=['Game of Thrones', 'A Song of Ice and Fire', 'Iron Thrones'])
